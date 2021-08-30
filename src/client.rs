82ef8da62927f86929331eab897f4b4e151799a8
YoEight <yo.eight@gmail.com>
2021-08-30 16:50:06 +0200

    Adapt gouth 0.2.1 code to 0.2.0.

--- src/client.rs
+++ src/client.rs
@@ -221,7 +221,7 @@ impl Client {
 
         let mut client = MetricServiceClient::with_interceptor(
             self.channel.clone(),
-            gouth::tonic::interceptor(),
+            tonic_ext::interceptor(),
         );
 
         if let Err(status) = client.create_time_series(tonic::Request::new(req)).await {
@@ -229,5 +229,26 @@ impl Client {
         }
 
         Ok(())
+    }
+}
+
+mod tonic_ext {
+    use gouth::Token;
+    use tonic::{metadata::MetadataValue, Interceptor, Request, Status};
+
+    macro_rules! map_err {
+        ($res:expr) => {
+            $res.map_err(|e| Status::unknown(e.to_string()))
+        };
+    }
+
+    pub fn interceptor() -> impl Into<Interceptor> {
+        let token = Token::new().expect("Token::new()");
+        move |mut req: Request<()>| {
+            let token = map_err!(token.header_value())?;
+            let meta = map_err!(MetadataValue::from_str(&*token))?;
+            req.metadata_mut().insert("authorization", meta);
+            Ok(req)
+        }
     }
 }
